157fd268345e67a5c0d5514a77bf2f1f28214afe
Ale2301 <alejo.2001@live.com.ar>
2022-04-15 20:17:30 -0300

    2nd commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+console.log("hola")
 function escribir(elemento,texto,delay) {
 	temporal = document.getElementById(elemento).innerHTML;
 	temporal = temporal.concat(texto.charAt(0));
@@ -11,12 +11,34 @@ function escribir(elemento,texto,delay) {
         repetirFuncion()
     }
 }
+function colorAzar(){
+    if (checkColor == "blanco"){
+        checkColor = "negro"
+        return ("negro")
+    }
+    else{
+        checkColor = "blanco"
+        return ("blanco")
+    }
+}
+function cambiarColores(colorcito){
+    let color = document.getElementById("autoTextTres")
+    color.classList.add(colorcito);
+    setTimeout(() => {
+        color.classList.remove(colorcito)
+        let colores = colorAzar()
+        cambiarColores(colores)
+    }, 1000);
+}
+let checkColor = 0;
 function repetirFuncion(){
     if (repetir == 0){
-        escribir("autoTextDos","Por esa razón entendemos que debemos tratar con el mismos respeto a nuestros más fieles compañeros...",40)
+        escribir("autoTextTres","Aladas huellas siempre acompaña.",40)
         repetir ++;
+        checkColor = "blanco";
+        cambiarColores("blanco")
     }
 }
 let temporal = 0;
 let repetir = 0;
-escribir("autoText","112 años de trayectoria en el rubro avalan nuestras prestación, siempre con calidad y distinción de forma personalizada.",40)
+escribir("autoText","112 años de trayectoria en el rubro avalan nuestra prestación,siempre con calidad y distinción, acompañando y entendiendo que debemos tratar con el mismo respeto a nuestros mas fieles compañeros, nuestras queridas mascotas.",40);
